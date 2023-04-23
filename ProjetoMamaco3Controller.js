029979e3b28ccd8587d6f903631053fb2d65f2a1
MeireYukie <129764347+MeireYukie@users.noreply.github.com>
2023-04-23 12:58:04 -0300

    Update ProjetoMamaco3Controller.js

--- ProjetoMamaco3Controller.js
+++ ProjetoMamaco3Controller.js
@@ -1,30 +1,8 @@
 ({
     doInit : function(component, event, helper) {
         helper.getLeads(component);
-    },
-   
-    /*
-    save : function(component, event, helper) {
-    component.find("edit").get("e.recordSave").fire();
-}
-    doUpdate : function(component, event, helper) {
-        var lead = event.getSource().get("v.value");
-        var action = component.get("c.updateLeadStatus");
-        action.setParams({ leadId : lead.Id });
-        action.setCallback(this, function(response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                var leads = component.get("v.leads");
-                var index = leads.indexOf(lead);
-                leads.splice(index, 1);
-                component.set("v.leads", leads);
-            }
-        });
-        $A.enqueueAction(action);
-    }*/
+    }    
 })
-
-     
 
 // O método "getLeads" é chamado no helper e recebe o componente como parâmetro.
 // Este é o método padrão que é executado quando o componente é inicializado.
